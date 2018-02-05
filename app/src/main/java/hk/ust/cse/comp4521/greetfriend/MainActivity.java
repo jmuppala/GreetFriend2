b8f721641edee92d32dcc69884fbd41ecb2eded9
Jogesh Muppala <jmuppala@hotmail.com>
2018-02-05 19:53:22 +0800

    GreetFriend Part 2

--- app/src/main/java/hk/ust/cse/comp4521/greetfriend/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/greetfriend/MainActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button greetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        greetButton = (Button) findViewById(R.id.greetButton);
+        greetButton.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +56,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        // get a reference to the TextView on the UI
+        TextView textMessage = (TextView) findViewById(R.id.textMessage);
+
+        //get a reference to the EditText so that we can read in the value typed
+        // by the user
+        EditText editFriendName = (EditText) findViewById(R.id.editFriendName);
+
+        // get the name of the friend typed in by the user in the EditText field
+        String friendName = editFriendName.getText().toString();
+
+        switch (view.getId()) {
+
+            case R.id.greetButton:
+
+                // set the string being displayed by the TextView to the greeting
+                // message for the friend
+                textMessage.setText("Good Day "+friendName+"!");
+
+                break;
+
+            default:
+
+                break;
+        }
     }
 }
