dad10479eb9dd7626bb95bb66333ab5d9ae6f100
Jogesh Muppala <jmuppala@hotmail.com>
2018-02-05 21:40:03 +0800

    GreetFriend with Activity Lifecycle

--- app/src/main/java/hk/ust/cse/comp4521/greetfriend/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/greetfriend/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button greetButton;
     String [] names;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +82,47 @@ public class MainActivity extends AppCompatActivity {
 
                 }
         );
+
+        Log.i(TAG, "in onCreate()");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "in onStart()");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "in onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "in onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "in onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "in onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "in onDestroy()");
     }
 
     @Override
--- app/src/main/java/hk/ust/cse/comp4521/greetfriend/ShowMessage.java
+++ app/src/main/java/hk/ust/cse/comp4521/greetfriend/ShowMessage.java
@@ -3,9 +3,12 @@ package hk.ust.cse.comp4521.greetfriend;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ShowMessage extends AppCompatActivity {
+
+    private static final String TAG = "ShowMessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,46 @@ public class ShowMessage extends AppCompatActivity {
 
         // set the text of the TextView to display the incoming greeting message
         textMessage.setText(message);
+
+        Log.i(TAG, "in onCreate()");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "in onStart()");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "in onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "in onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "in onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "in onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "in onDestroy()");
     }
 }
